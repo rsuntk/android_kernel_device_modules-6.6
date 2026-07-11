e6f62d706dbd1fc660e3e987787c12ef9e3f2754
ris <rissu.ntk@gmail.com>
2026-07-11 12:41:00 +0700

    kleaf: stamp
    Signed-off-by: ris <rissu.ntk@gmail.com>

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ fi
 KLEAF_DIST_TARGET=//${DEVICE_MODULES_DIR}:${PROJECT}_${build_scope}_dist.${MODE}
 
 KLEAF_OUT=("--output_user_root=${OUT_DIR} --output_base=${OUT_DIR}/bazel/output_user_root/output_base")
-KLEAF_ARGS=("${DEBUG_ARGS} ${SANDBOX_ARGS} \
+KLEAF_ARGS=("${DEBUG_ARGS} --config=stamp ${SANDBOX_ARGS} \
 	--repo_manifest=${ROOT_DIR}/${DEVICE_MODULES_DIR}/fake_manifest.xml \
 	--experimental_writable_outputs \
 	--noenable_bzlmod \
