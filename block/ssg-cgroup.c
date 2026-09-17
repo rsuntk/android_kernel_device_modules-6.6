fb1789a33256c36c1423d96e43f5106d66bd72bd
rssu. <rissu.ntk@gmail.com>
2026-09-17 19:11:51 +0700

    Revert "profile: Update autoFDO profile"
    This reverts commit 0987787d155af87eaf22999b5ab7fc0272137b88.
    
    Signed-off-by: rssu. <rissu.ntk@gmail.com>

--- block/ssg-cgroup.c
+++ block/ssg-cgroup.c
@@ -16,6 +16,8 @@
 
 static struct blkcg_policy ssg_blkcg_policy;
 
+
+
 #define CPD_TO_SSG_BLKCG(_cpd) \
 	container_of((_cpd), struct ssg_blkcg, cpd)
 #define BLKCG_TO_SSG_BLKCG(_blkcg) \
@@ -31,6 +33,8 @@ static struct blkcg_policy ssg_blkcg_policy;
 #define MIN_SCHED_TAGS		32
 #define MIN_AVAILABLE_RATIO	30 /* note: max_async_write_ratio is 25% */
 #define MAX_AVAILABLE_RATIO	100
+
+
 
 static struct blkcg_policy_data *ssg_blkcg_cpd_alloc(gfp_t gfp)
 {
